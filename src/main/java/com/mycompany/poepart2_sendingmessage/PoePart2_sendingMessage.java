bef4b91b1c42a619b570af3089779842116edca3
deepl <deepl@OmphileMoiloa>
2026-05-24 01:02:20 +0200

    linking my project to Github CREATING METHODS

---
+++ src/main/java/com/mycompany/poepart2_sendingmessage/PoePart2_sendingMessage.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poepart2_sendingmessage;
+
+/**
+ *
+ * @author deepl
+ */
+public class PoePart2_sendingMessage {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
